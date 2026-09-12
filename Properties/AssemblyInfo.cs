a1af25a633120413da74a345233918e53a5d1f46
Andrew <92362270+andrasmining@users.noreply.github.com>
2026-09-12 20:20:04 +0200

    test: expose internals to diagnostics suite

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,6 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2022 4RTools")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: InternalsVisibleTo("Vanilla.Diagnostics.Tests")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("a7d78b00-6ce1-4b4d-90a3-1d2e93c7f68a")]
