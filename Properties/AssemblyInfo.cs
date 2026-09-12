d8258b59e6dc36412c02173c7d9db3f304c64e05
Andrew <92362270+andrasmining@users.noreply.github.com>
2026-09-12 20:29:44 +0200

    release: bump 4RTools Vanilla to 0.6.8

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("a7d78b00-6ce1-4b4d-90a3-1d2e93c7f68a")]
 
-[assembly: AssemblyVersion("0.6.7.0")]
-[assembly: AssemblyFileVersion("0.6.7.0")]
+[assembly: AssemblyVersion("0.6.8.0")]
+[assembly: AssemblyFileVersion("0.6.8.0")]
